0adf51fe094cb777f46e47bfe4baf376ca1b6245
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2023-09-07 01:31:32 +0000

    Display tasks

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def homepage():
+    tasks = []
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        tasks.append(request.form.get("task"))
+        return render_template("index.html", tasks=tasks)
 
 
 @app.route("/create", methods=["POST"])
