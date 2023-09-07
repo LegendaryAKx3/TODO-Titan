b29393f80b12a586a3537f3a352ae1c313e53ed2
Selyss <99344963+Selyss@users.noreply.github.com>
2023-09-06 20:51:01 -0400

    Merge: layout

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    return render_template("task_success.html", name=request.args.get("name", "world"))
