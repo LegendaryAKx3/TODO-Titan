239e1c5e8ffce969ebb6f6c2978bd799842c74ac
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2023-09-08 00:51:21 +0000

    Change homepage code

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = SQL("sqlite:///todotitan.db")
 
 # SQL table structure
 # CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username UNIQUE TEXT NOT NULL, hash TEXT NOT NULL);
-# CREATE TABLE tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, task_text TEXT NOT NULL);
+# CREATE TABLE tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, task_text TEXT NOT NULL, user_id INTEGER NOT NULL);
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
@@ -18,7 +18,7 @@ def homepage():
         return render_template("index.html")
     else:
         tasks.append(request.form.get("task"))
-        return render_template("index.html", tasks=tasks)
+        return redirect("/")
 
 
 @app.route("/delete", methods=["POST"])
