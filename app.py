ce5a2a0180587bec960e6d0df137959f520a83ef
Selyss <99344963+Selyss@users.noreply.github.com>
2023-09-10 18:53:42 -0400

    Update: clean from branch

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, session
-from flask_session import Session
-from cs50 import SQL
-from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
+
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -14,11 +16,13 @@ Session(app)
 # Initialize database with cs50 library
 db = SQL("sqlite:///todotitan.db")
 
+
 # SQL table structure
 # CREATE TABLE accounts (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL);
 # CREATE TABLE tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, task_text TEXT NOT NULL, uuid INTEGER NOT NULL);
 def login_required(f):
-    '''Code from CS50 finance'''
+    """Code from CS50 finance"""
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("uuid") is None:
@@ -27,8 +31,10 @@ def login_required(f):
 
     return decorated_function
 
+
 def error(message):
     return render_template("error.html", message=message)
+
 
 @app.route("/", methods=["GET", "POST"])
 @login_required
@@ -47,21 +53,27 @@ def delete():
     # TODO remove task from database
     return redirect("/")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Code based on login from CS50 finance"""
-    # Reset session
     session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Check username and password are submitted
+        # Check both username and password are submitted
         if (not request.form.get("username")) or (not request.form.get("password")):
             return error("Submitted fields cannot be blank")
-        
-        user = db.execute("SELECT * FROM accounts WHERE username = ?", request.form.get("username"))
+
+        user = db.execute(
+            "SELECT * FROM accounts WHERE username = ?", request.form.get("username")
+        )
         # Ensure username exists and password is correct
-        if len(user) != 1 or not check_password_hash(user[0]["hash"], request.form.get("password")):
+
+        # HACK: len(user) != 1?
+        if len(user) != 1 or not check_password_hash(
+            user[0]["hash"], request.form.get("password")
+        ):
             return error("Invalid login information")
 
         # store account login status until session is cleared
@@ -70,32 +82,36 @@ def login():
 
     else:
         return render_template("login.html")
-    
+
+
 @app.route("/logout")
 def logout():
-    '''Code based on logout from CS50 finance'''
+    """Code based on logout from CS50 finance"""
     session.clear()
     return redirect("/")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user (My code from CS50 Finance)"""
+    """Register user (Personal code from CS50 Finance)"""
     if request.method == "POST":
-        # if form was submitted via post, get form info and check validity
+        # get form info and check validity
+
         username = request.form.get("username")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
-        # check username valididty
+
         if not username:
             return error("Username field cannot be empty")
 
         for entry in db.execute("SELECT username FROM accounts;"):
             if username == entry["username"]:
-                return error("Username is already taken")
+                return error("Username is Already Taken")
 
         # check password valididty
         if password != confirmation or (not password) or (not confirmation):
             return error("Invalid Password or Confirmation")
+
         elif len(password) < 8:
             return error("Password Must be at Least 8 Characters Long")
 
@@ -105,7 +121,4 @@ def register():
             generate_password_hash(password),
         )
         return redirect("/")
-    else:
-        return render_template("register.html")
-
-
+    return render_template("register.html")
